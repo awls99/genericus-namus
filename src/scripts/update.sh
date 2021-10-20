693008c52dbfae2a3fbc528da68b89c8a0ee5f2e
Arthur Silva <awls99@gmail.com>
2021-10-20 21:47:36 +0100

    update script

--- src/scripts/update.sh
+++ src/scripts/update.sh
@@ -1,3 +1,8 @@
+git pull
 npm ci;
 tsc --resolveJsonModule src/scripts/generator.ts
 node src/scripts/generator.js
+npm run deploy
+git add src/json/roster.json
+git -m "automatic roster update"
+git push
