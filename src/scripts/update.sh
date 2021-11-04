7cf182231b0f8c1e0470659a6cac49c7d8aa0dc8
Arthur Silva <awls99@gmail.com>
2021-11-04 15:47:58 +0000

    path hardcode

--- src/scripts/update.sh
+++ src/scripts/update.sh
@@ -1,8 +1,8 @@
 git pull;
-npm ci;
-tsc --resolveJsonModule src/scripts/generator.ts;
-node src/scripts/generator.js;
-npm run deploy;
+/home/pi/.nvm/versions/node/v14.17.0/bin/npm ci;
+/home/pi/.nvm/versions/node/v14.17.0/bin/tsc --resolveJsonModule src/scripts/generator.ts;
+/home/pi/.nvm/versions/node/v14.17.0/bin/node src/scripts/generator.js;
+/home/pi/.nvm/versions/node/v14.17.0/bin/npm run deploy;
 git add src/json/roster.json;
-git -m "automatic roster update";
+git commit -m "automatic roster update";
 git push;
